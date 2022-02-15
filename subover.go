46fd4660a25ad12dde9d01246980b2431a8e6e8b
PikPikcU <60111811+pikpikcu@users.noreply.github.com>
2022-02-15 10:35:51 -0500

    Update subover.go

--- subover.go
+++ subover.go
@@ -39,7 +39,7 @@ var (
 )
 
 func InitializeProviders() {
-    raw, err := ioutil.ReadFile("providers.json")
+    raw, err := ioutil.ReadFile("~/bugbounty/tools/file/SubOver/providers.json")
     if err != nil {
         fmt.Println(err.Error())
         os.Exit(1)
